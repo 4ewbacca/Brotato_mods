8c4b59f67ebd82e39aa36a5321bcfd7959ec4c81
4ewbacca <95045595+4ewbacca@users.noreply.github.com>
2023-08-25 22:22:12 +0300

    Add files via upload

--- 4ewbakka-Bullet_Character/mod_main.gd
+++ 4ewbakka-Bullet_Character/mod_main.gd
@@ -1,10 +1,10 @@
 extends Node
 
-const MENUSTRING_LOG = "4ewbakka-Bullet_Character"
+const MENUSTRING_LOG = "Darkly77-MenuString"
 
 func _init(modLoader):
 	ModLoaderLog.info("Init", MENUSTRING_LOG)
-	ModLoaderMod.install_script_extension("res:/tree/main/4ewbakka-Bullet_Character/extensions/ui/menus/pages/main_menu.gd")
+	ModLoaderMod.install_script_extension("res://mods-unpacked/Darkly77-MenuString/extensions/ui/menus/pages/main_menu.gd")
 
 func _ready():
 	ModLoaderLog.info("Ready", MENUSTRING_LOG)
