384ce5d48bae536a62f2e906e20ebac138393828
4ewbacca <95045595+4ewbacca@users.noreply.github.com>
2023-08-25 23:04:41 +0300

    Add files via upload

---
+++ mod_main.gd
@@ -0,0 +1,10 @@
+extends Node
+
+const MENUSTRING_LOG = "4ewbakka-Bullet_Character"
+
+func _init(modLoader):
+	ModLoaderLog.info("Init", MENUSTRING_LOG)
+	ModLoaderMod.install_script_extension("res://mods-unpacked/4ewbakka-Bullet_Character/extensions/ui/menus/pages/main_menu.gd")
+
+func _ready():
+	ModLoaderLog.info("Ready", MENUSTRING_LOG)
